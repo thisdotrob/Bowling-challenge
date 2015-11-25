1b87c4f4139a7f6d99229563453dd67f310b62d0
thisdotrob <thisdotrob@gmail.com>
2015-11-25 10:50:14 +0000

    Pass initial scoring test

---
+++ public/js/jquery.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('#roll_button').click(function(){
+    $('#frame0roll1').text('9');
+  });
+});
--- test/test.js
+++ test/test.js
@@ -2,23 +2,37 @@ var Browser = require('zombie');
 
 Browser.localhost('example.com', 3000);
 
-describe('User visits index page', function() {
+describe('User visits index page', function(){
   var browser = new Browser();
 
-  before(function(done) {
+  before(function(done){
     browser.visit('/', done);
   });
 
-  it('should display the welcome message', function() {
-    browser.assert.text('h1', 'Welcome to Bowling');
+  it('should display the welcome message', function(){
+    browser.assert.text('h1', 'Let\'s bowl!');
   });
 
-  it('should have the correct page title', function() {
+  it('should have the correct page title', function(){
     browser.assert.text('title', 'Bowling');
   });
 
-  it('should show the accuracy prompt', function() {
+  it('should show the accuracy prompt', function(){
     browser.assert.text('#accuracy_form', 'Accuracy: Roll!');
+  });
+
+  describe('User fills in the accuracy form', function(){
+
+    before(function(done){
+      browser
+        .fill('accuracy_field', '9')
+        .pressButton('roll_button', done);
+    });
+
+    it('should display the roll in the scoring table', function(){
+      browser.assert.text('#frame0roll1', '9');
+    });
+
   });
 
 });
